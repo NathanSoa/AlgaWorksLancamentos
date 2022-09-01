7381583dd2b3b78c360ccf9e163b782324cdbc34
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-01 19:42:35 -0300

    refactor: post request for '/categoria' now return a response entity and location header

--- src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
@@ -4,8 +4,13 @@ import br.com.algaworks.api.lancamentosapi.Model.Categoria;
 import br.com.algaworks.api.lancamentosapi.Repository.ICategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,14 +20,20 @@ public class CategoriaController {
     @Autowired
     private ICategoriaRepository iCategoriaRepository;
 
+
     @GetMapping
     public List<Categoria> listarTodasCategorias(){
         return iCategoriaRepository.findAll();
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void cadastrarNovaCategoria(@RequestBody Categoria categoria){
-        iCategoriaRepository.save(categoria);
+    public ResponseEntity<Categoria> cadastrarNovaCategoria(@RequestBody Categoria categoria, HttpServletResponse response){
+        Categoria categoriaSalva = iCategoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 }
