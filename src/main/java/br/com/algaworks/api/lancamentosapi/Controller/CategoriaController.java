ded33841da5d01b28c33b9e101c42ecc8a2c2a88
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-01 19:09:50 -0300

    add autowired annotation

--- src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
@@ -2,6 +2,7 @@ package br.com.algaworks.api.lancamentosapi.Controller;
 
 import br.com.algaworks.api.lancamentosapi.Model.Categoria;
 import br.com.algaworks.api.lancamentosapi.Repository.ICategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
+    @Autowired
     private ICategoriaRepository iCategoriaRepository;
 
     @GetMapping
