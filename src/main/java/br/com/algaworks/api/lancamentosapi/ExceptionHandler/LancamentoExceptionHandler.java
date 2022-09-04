aa618d0a0a23614523c118db3e3d13922a17e241
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 13:13:18 -0300

    add handler to null values and string that are off size

--- src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
@@ -7,23 +7,56 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class LancamentoExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+    private List<Erro> erros = new ArrayList<>();
+    private String mensagemParaDesenvolvedor = "";
+    private String mensagemParaUsuario ="";
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String mensagemParaUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-        String mensagemParaDesenvolvedor = ex.getCause().toString();
-        return handleExceptionInternal(ex, new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+        reiniciaListaDeErros();
+        mensagemParaUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        mensagemParaDesenvolvedor = ex.getCause().toString();
+        erros = Arrays.asList(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        reiniciaListaDeErros();
+        erros = criaListaDeErros(ex.getBindingResult());
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criaListaDeErros(BindingResult bindingResult){
+        List<Erro> erros = new ArrayList<>();
+        for(FieldError fieldError : bindingResult.getFieldErrors()){
+            mensagemParaUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            mensagemParaDesenvolvedor = fieldError.toString();
+            erros.add(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
+        }
+        return erros;
+    }
+
+    private void reiniciaListaDeErros(){
+        if(!erros.isEmpty())
+            erros.clear();
+    }
     private static class Erro {
         private String mensagemUsuario;
         private String mensagemDesenvolvedor;
