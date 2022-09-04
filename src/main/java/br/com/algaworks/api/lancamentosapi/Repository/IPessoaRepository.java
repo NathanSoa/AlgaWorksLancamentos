0bff329ac3387accd9e6ae5bdd945fd22837cfef
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 13:47:19 -0300

    add repository for person

--- src/main/java/br/com/algaworks/api/lancamentosapi/Repository/IPessoaRepository.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Repository/IPessoaRepository.java
@@ -1,2 +1,10 @@
-package br.com.algaworks.api.lancamentosapi.Repository;public interface IPessoaRepository {
+package br.com.algaworks.api.lancamentosapi.Repository;
+
+import br.com.algaworks.api.lancamentosapi.Model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPessoaRepository extends JpaRepository<Pessoa, Long> {
+
 }
