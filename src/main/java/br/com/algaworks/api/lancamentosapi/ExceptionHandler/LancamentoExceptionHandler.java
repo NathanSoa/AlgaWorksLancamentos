7556267af0508284b835ae114a9871fb8f23a0dd
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 14:32:45 -0300

    add handler to EmptyResultDataAccessException

--- src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.algaworks.api.lancamentosapi.ExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -31,7 +34,7 @@ public class LancamentoExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         reiniciaListaDeErros();
         mensagemParaUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-        mensagemParaDesenvolvedor = ex.getCause().toString();
+        mensagemParaDesenvolvedor = ex.toString();
         erros = Arrays.asList(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
@@ -41,6 +44,15 @@ public class LancamentoExceptionHandler extends ResponseEntityExceptionHandler {
         reiniciaListaDeErros();
         erros = criaListaDeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+       reiniciaListaDeErros();
+        mensagemParaUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+        mensagemParaDesenvolvedor = ex.toString();
+        erros = Arrays.asList(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
+       return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     private List<Erro> criaListaDeErros(BindingResult bindingResult){
