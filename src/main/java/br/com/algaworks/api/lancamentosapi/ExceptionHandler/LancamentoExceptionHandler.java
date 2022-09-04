4cf7e2861e4392fca4e2a4a6bce852eb45c11d5f
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 15:36:57 -0300

    refactor: add list to each method

--- src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/ExceptionHandler/LancamentoExceptionHandler.java
@@ -26,29 +26,28 @@ public class LancamentoExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
-    private List<Erro> erros = new ArrayList<>();
     private String mensagemParaDesenvolvedor = "";
     private String mensagemParaUsuario ="";
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        reiniciaListaDeErros();
+        List<Erro> erros;
         mensagemParaUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-        mensagemParaDesenvolvedor = ex.getCause().toString();
+        mensagemParaDesenvolvedor = (ex.getCause() != null) ? ex.getCause().toString() : ex.toString();
         erros = Arrays.asList(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        reiniciaListaDeErros();
+        List<Erro> erros;
         erros = criaListaDeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler({EmptyResultDataAccessException.class})
     protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
-       reiniciaListaDeErros();
+        List<Erro> erros;
         mensagemParaUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
         mensagemParaDesenvolvedor = ex.toString();
         erros = Arrays.asList(new Erro(mensagemParaUsuario, mensagemParaDesenvolvedor));
@@ -65,10 +64,6 @@ public class LancamentoExceptionHandler extends ResponseEntityExceptionHandler {
         return erros;
     }
 
-    private void reiniciaListaDeErros(){
-        if(!erros.isEmpty())
-            erros.clear();
-    }
     private static class Erro {
         private String mensagemUsuario;
         private String mensagemDesenvolvedor;
