c988a0ab18c6ad34194de276bba32ef451300759
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 13:04:44 -0300

    add @Valid annotation

--- src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -24,7 +25,7 @@ public class CategoriaController {
     }
 
     @PostMapping
-    public ResponseEntity<Categoria> cadastrarNovaCategoria(@RequestBody Categoria categoria, HttpServletResponse response){
+    public ResponseEntity<Categoria> cadastrarNovaCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response){
         Categoria categoriaSalva = iCategoriaRepository.save(categoria);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
