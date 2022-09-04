e02b722ab893fc4cc46e751718b2692b05f181d4
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-04 13:46:13 -0300

    add Person entity with embed address

---
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Model/Pessoa.java
@@ -0,0 +1,56 @@
+package br.com.algaworks.api.lancamentosapi.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @NotNull
+    @Size(min = 3, max = 200)
+    private String nome;
+
+    @NotNull
+    private boolean ativo;
+
+    @Embedded
+    private Endereco endereco;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
