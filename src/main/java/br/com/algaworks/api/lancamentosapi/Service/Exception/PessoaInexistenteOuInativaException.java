048a849267be580571a02b0c128b1dd25b2f8cdd
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-06 20:19:35 -0300

    add new exception to use when invalid/inactive person is sent

---
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Service/Exception/PessoaInexistenteOuInativaException.java
@@ -0,0 +1,6 @@
+package br.com.algaworks.api.lancamentosapi.Service.Exception;
+
+public class PessoaInexistenteOuInativaException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+}
