8acca3406eab22a9898f0d03d1248fd3a6b2af91
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-06 21:14:12 -0300

    add Lancamento Filter class

--- src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Filter/LancamentoFilter.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Filter/LancamentoFilter.java
@@ -1,2 +1,40 @@
-package br.com.algaworks.api.lancamentosapi.Repository.Filter;public class LancamentoFilter {
+package br.com.algaworks.api.lancamentosapi.Repository.Filter;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class LancamentoFilter {
+
+    private String descricacao;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataVencimentoDe;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataVencimentoAte;
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public void setDescricacao(String descricacao) {
+        this.descricacao = descricacao;
+    }
+
+    public LocalDate getDataVencimentoDe() {
+        return dataVencimentoDe;
+    }
+
+    public void setDataVencimentoDe(LocalDate dataVencimentoDe) {
+        this.dataVencimentoDe = dataVencimentoDe;
+    }
+
+    public LocalDate getDataVencimentoAte() {
+        return dataVencimentoAte;
+    }
+
+    public void setDataVencimentoAte(LocalDate dataVencimentoAte) {
+        this.dataVencimentoAte = dataVencimentoAte;
+    }
 }
