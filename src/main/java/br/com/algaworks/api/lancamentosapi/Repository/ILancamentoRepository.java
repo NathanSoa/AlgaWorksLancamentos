ad98668e1df89d8d731b01e5df9048dab1ff2025
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-06 21:14:49 -0300

    add extends to new interface

--- src/main/java/br/com/algaworks/api/lancamentosapi/Repository/ILancamentoRepository.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Repository/ILancamentoRepository.java
@@ -1,10 +1,11 @@
 package br.com.algaworks.api.lancamentosapi.Repository;
 
 import br.com.algaworks.api.lancamentosapi.Model.Lancamento;
+import br.com.algaworks.api.lancamentosapi.Repository.Lancamento.LancamentoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ILancamentoRepository extends JpaRepository<Lancamento, Long> {
+public interface ILancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 }
