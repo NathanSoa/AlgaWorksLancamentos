019103d0ed0c83f1783dedb58a0db84b7f1ee6e0
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-06 21:13:48 -0300

    add builder to generate filtered query

--- src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Lancamento/LancamentoRepositoryQuery.java
@@ -1,2 +1,11 @@
-package br.com.algaworks.api.lancamentosapi.Repository.Lancamento;public interface LancamentoRepositoryQuery {
+package br.com.algaworks.api.lancamentosapi.Repository.Lancamento;
+
+import br.com.algaworks.api.lancamentosapi.Model.Lancamento;
+import br.com.algaworks.api.lancamentosapi.Repository.Filter.LancamentoFilter;
+
+import java.util.List;
+
+public interface LancamentoRepositoryQuery {
+
+    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
 }
--- src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Lancamento/LancamentoRepositoryQueryImpl.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Repository/Lancamento/LancamentoRepositoryQueryImpl.java
@@ -1,2 +1,67 @@
-package br.com.algaworks.api.lancamentosapi.Repository.Lancamento;public class LancamentoRepositoryQueryImpl {
+package br.com.algaworks.api.lancamentosapi.Repository.Lancamento;
+
+import br.com.algaworks.api.lancamentosapi.Model.Lancamento;
+import br.com.algaworks.api.lancamentosapi.Repository.Filter.LancamentoFilter;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LancamentoRepositoryQueryImpl implements LancamentoRepositoryQuery{
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
+        Root<Lancamento> root = criteria.from(Lancamento.class);
+
+        Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+        criteria.where(predicates);
+
+        TypedQuery<Lancamento> query = manager.createQuery(criteria);
+        return query.getResultList();
+    }
+
+    private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder, Root<Lancamento> root) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if(descricaoNaoEstaNula(lancamentoFilter)){
+            predicates.add(builder.like(
+                    builder.lower(
+                            root.get("descricao")),
+                    "%" + lancamentoFilter.getDescricacao().toLowerCase() + "%")
+            );
+        }
+
+        if(dataVencimentoDeNaoEstaNula(lancamentoFilter)){
+            //predicates.add(e);
+        }
+
+        if(dataVencimentoAteNaoEstaNula(lancamentoFilter)){
+            //predicates.add(e);
+        }
+        return predicates.toArray(new Predicate[predicates.size()]);
+    }
+
+    private Boolean descricaoNaoEstaNula(LancamentoFilter lancamentoFilter){
+        return !StringUtils.isEmpty(lancamentoFilter.getDescricacao());
+    }
+
+    private Boolean dataVencimentoDeNaoEstaNula(LancamentoFilter lancamentoFilter){
+        return lancamentoFilter.getDataVencimentoDe() != null;
+    }
+
+    private Boolean dataVencimentoAteNaoEstaNula(LancamentoFilter lancamentoFilter){
+        return lancamentoFilter.getDataVencimentoAte() != null;
+    }
 }
