5992bec68c99f6309dd8a5918f2b6b1337cb41d5
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-10 20:35:15 -0300

    add filter to automatically pass the cookie on new request for access token with refresh token

--- src/main/java/br/com/algaworks/api/lancamentosapi/Token/RefreshTokenCookiePreProcessorFilter.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Token/RefreshTokenCookiePreProcessorFilter.java
@@ -2,15 +2,19 @@ package br.com.algaworks.api.lancamentosapi.Token;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequestWrapper;
 import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Map;
 
 @Component
-public class RefreshTokenPreProcessorFilter implements Filter {
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -18,15 +22,17 @@ public class RefreshTokenPreProcessorFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
         if(request.getCookies() != null
-            && request.getAttribute("refresh_token") != null
-            && request.getRequestURI().equals("/oauth/token")){
+            && request.getParameter("grant_type").equals("refresh_token")
+            && request.getRequestURI().equalsIgnoreCase("/oauth/token")){
 
             for(Cookie cookie : request.getCookies()){
                 if(cookie.getName().equals("refreshToken")){
-
+                    String refreshToken = cookie.getValue();
+                    request = new MyServletRequestWraper(request, refreshToken);
                 }
             }
         }
+        filterChain.doFilter(request, servletResponse);
     }
 
     @Override
@@ -39,14 +45,21 @@ public class RefreshTokenPreProcessorFilter implements Filter {
 
     }
 
-    private static class MyServletWraper extends HttpServletRequestWrapper{
+    private static class MyServletRequestWraper extends HttpServletRequestWrapper {
 
+        private String refreshToken;
 
-        public MyServletWraper(HttpServletRequest request, String refreshToken) {
+        public MyServletRequestWraper(HttpServletRequest request, String refreshToken) {
             super(request);
+            this.refreshToken = refreshToken;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap(){
             ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
             map.put("refresh_token", new String[] {refreshToken});
             map.setLocked(true);
+            return map;
         }
     }
 }
