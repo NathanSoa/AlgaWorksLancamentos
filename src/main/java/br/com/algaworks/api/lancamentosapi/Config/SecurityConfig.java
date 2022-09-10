ebf0091c01ec14eb0f228b394a9f62c71eddbd3b
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-09-10 17:56:57 -0300

    add test authentication

--- src/main/java/br/com/algaworks/api/lancamentosapi/Config/SecurityConfig.java
+++ src/main/java/br/com/algaworks/api/lancamentosapi/Config/SecurityConfig.java
@@ -1,2 +1,27 @@
-package br.com.algaworks.api.lancamentosapi.Config;public class SecurityConfig {
+package br.com.algaworks.api.lancamentosapi.Config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin").roles("ROLES");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest()
+                .authenticated().and()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .csrf().disable();
+    }
 }
